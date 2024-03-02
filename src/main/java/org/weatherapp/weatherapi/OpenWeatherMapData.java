3e8d86ee40f0fc564ffcae20acdc47e7b1ef301e
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2024-03-02 19:01:03 +0100

    optimize Code and add Description

--- src/main/java/org/weatherapp/weatherapi/OpenWeatherMapData.java
+++ src/main/java/org/weatherapp/weatherapi/OpenWeatherMapData.java
@@ -14,41 +14,43 @@ public class OpenWeatherMapData {
 
 
     public String getWeatherData(String urlString, String city, String countryCode, String limit, String apiKey) throws IOException {
+        //get location Information's
         URL url = new URL(urlString + "q=" + city + "," + countryCode + "&limit=" + limit + "&appid=" + apiKey);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-
+        HttpURLConnection connection = connect(url);
         StringBuilder output = new StringBuilder();
-        if (con.getResponseCode() != 200) {
-            throw new RuntimeException("HttpResponseCode: " + con.getResponseCode());
+        if (connection.getResponseCode() != 200) {
+            throw new RuntimeException("HttpResponseCode: " + connection.getResponseCode());
         } else {
             Scanner scanner = new Scanner(url.openStream());
             while (scanner.hasNextLine()) {
                 output.append(scanner.nextLine());
             }
         }
+        //deletes the Array Syntax, cause readTree can't handle that
         output.deleteCharAt(0);
         output.deleteCharAt(output.length() - 1);
+        // filter the JSON Data for lat and lon (coordination's)
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(output.toString());
         String lat = jsonNode.get("lat").asText();
         String lon = jsonNode.get("lon").asText();
+        //get Weather Information's
         url = new URL("https://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=metric");
-        con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-        if (con.getResponseCode() != 200) {
-            throw new RuntimeException("HttpResponseCode: " + con.getResponseCode());
+        connection = connect(url);
+        if (connection.getResponseCode() != 200) {
+            throw new RuntimeException("HttpResponseCode: " + connection.getResponseCode());
         } else {
             Scanner scanner = new Scanner(url.openStream());
             while (scanner.hasNextLine()) {
                 output.append(scanner.nextLine());
             }
         }
-        //todo transform the data into Json-format and extract the location data and put the into actually weatherData API
         return output.toString();
     }
+
+    /**
+     * this methode is only for now and will be changed in future Versions, APIKey would be requested by user input and saved to a config-File.
+     */
 
     private String getApiKey(String path) throws FileNotFoundException {
         File file = new File(path);
@@ -60,15 +62,18 @@ public class OpenWeatherMapData {
         return output.toString();
     }
 
+    private HttpURLConnection connect(URL url) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.connect();
+        return con;
+    }
+
     //this main is only for testing
     public static void main(String[] args) throws IOException {
         OpenWeatherMapData Test = new OpenWeatherMapData();
         //just for testing
-        String result = Test.getWeatherData("http://api.openweathermap.org/geo/1.0/direct?",
-                "Roetgen",
-                "DE",
-                "1",
-                Test.getApiKey("src/main/resources/other/apiKeyStorage.txt"));
+        String result = Test.getWeatherData("http://api.openweathermap.org/geo/1.0/direct?", "Roetgen", "DE", "1", Test.getApiKey("src/main/resources/other/apiKeyStorage.txt"));
         System.out.println(result);
     }
 
